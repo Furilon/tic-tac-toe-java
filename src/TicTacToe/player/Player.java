ecaa9b80e98c939da53587e4520000bbf938ef9e
Mykyta Medvediev <arlekin8881@gmail.com>
2023-08-20 16:13:12 -0700

    Added a getSign method to Player.

--- src/TicTacToe/player/Player.java
+++ src/TicTacToe/player/Player.java
@@ -3,23 +3,27 @@ package TicTacToe.player;
 import TicTacToe.game.Move;
 
 public class Player {
-    private Move sign;
+    private char sign;
     private boolean move;
 
     public Player(Move sign) {
-        this.sign = sign;
+        this.sign = sign.getSign();
         this.move = false;
     }
 
-    public boolean isMyMove() {
+    public char getSign() {
+        return this.sign;
+    }
+
+    public boolean isMove() {
         return this.move;
     }
 
-    public void changeMyMove() {
+    public void switchMove() {
         this.move = !this.move;
     }
 
     public String toString() {
-        return "Player " + this.sign.getSign();
+        return "Player " + this.sign;
     }
 }
