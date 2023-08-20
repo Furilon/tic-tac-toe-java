0f41c2126a5ff07d073a9f0f2ab371762a7135a6
Mykyta Medvediev <arlekin8881@gmail.com>
2023-08-20 16:08:26 -0700

    Created a switchMoves function.

--- src/TicTacToe/game/GameController.java
+++ src/TicTacToe/game/GameController.java
@@ -1,10 +1,27 @@
 package TicTacToe.game;
 
 import TicTacToe.player.Player;
+import TicTacToe.game.Move;
 
 public class GameController {
+    private char moveSign;
+
+    public GameController(){
+        this.moveSign = Move.X.getSign();
+    }
 
     public Player createPlayer(Move sign) {
         return new Player(sign);
+    }
+
+    public void switchMoves(Player playerX, Player playerY) {
+        if (playerX.isMyMove()) {
+            this.moveSign = Move.Y.getSign();
+        } else {
+            this.moveSign = Move.X.getSign();
+        }
+
+        playerX.changeMyMove();
+        playerY.changeMyMove();
     }
 }
